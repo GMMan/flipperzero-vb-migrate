b0311da98860dbbcb7cafd86cb3dad2e0977ee27
Yukai Li <yukaili.geek@gmail.com>
2022-11-21 15:03:27 -0700

    Add license

--- scenes/vb_migrate_scene_delete_captures.c
+++ scenes/vb_migrate_scene_delete_captures.c
@@ -1,3 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+// VB Lab Migration Assistant for Flipper Zero
+// Copyright (C) 2022  cyanic
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #include "../vb_migrate_i.h"
 
 static void vb_migrate_scene_delete_captures_widget_callback(
